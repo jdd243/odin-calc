32e2ffb05fa1f3fadfab66c277f8a515fd0e1498
jdd243 <151496031+jdd243@users.noreply.github.com>
2025-10-18 09:29:12 -0600

    script
    added button functions

--- script.js
+++ script.js
@@ -31,14 +31,70 @@ const divide = function(a, b) {
 //Math using functions
 const compute = (a, operator, b) => {
 
-    const x = parseFloat(aStr);
-    const y = parseFloat(bStr);
+    const x = parseFloat(a);
+    const y = parseFloat(b);
 
     if (Number.isNaN(x) || Number.isNaN(y)) return "Error"
     if (operator === "+") return add(x, y);
-    if (op === "-") return subtract(x, y);
-    if (op === "*") return multiply([x, y]);
-    if (op === "/") return divide(x, y);
+    if (operator === "-") return subtract(x, y);
+    if (operator === "*") return multiply([x, y]);
+    if (operator === "/") return divide(x, y);
     return "Error";
 };
 
+//Input
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const key = btn.dataset.key;
+
+        //number input
+        if (!isNaN(key) || key ===".") {
+            if (!operator) {
+                a += key;
+                value(a);
+            }
+            else {
+                b += key;
+                value(b)
+            }
+            return;
+        }
+        //operator input
+        if (["+", "-", "*", "/"].includes(key)) {
+
+            if(a && !b) {
+                operator = key;
+                return;
+            }
+        }
+        //Equals input
+        if (key === "=") {
+            if (a && operator && b) {
+                const result = compute(a,operator, b);
+                value(result);
+                a = result.toString();
+                operator = "";
+                b = "";
+            }
+        }
+        //Clear input
+        if (key === "clear") {
+            a = "";
+            operator = "";
+            b = "";
+            value("0");
+            return;
+        }
+        //Delete input
+        if (key === "delete") {
+            if(!operator) {
+                a = a.slice(0, -1);
+                value (a || "0");
+            } else {
+                b = b.slice(0 ,-1);
+                value(b || "0");
+            }
+            return;
+        }
+    })    
+})
